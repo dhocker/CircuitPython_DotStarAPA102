47df9eac617ded2fb3c932a769f0c6231fd43243
Dave Hocker <AtHomeX10@gmail.com>
2018-10-11 11:24:00 -0500

    Polish setup file. Add license statement.

--- setup.py
+++ setup.py
@@ -1,12 +1,38 @@
-"""A setuptools based setup module.
-
-See:
-https://packaging.python.org/en/latest/distributing.html
-https://github.com/pypa/sampleproject
-"""
+# coding: utf-8
+# 
+# A setuptools based setup module. This code was derived from the
+# template produced by the Adafruit CookieCutter app. 
+#
+# See:
+# https://packaging.python.org/en/latest/distributing.html
+# https://github.com/pypa/sampleproject
+#
+# Copyright © 2018 Dave Hocker (email: AtHomeX10@gmail.com)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the LICENSE.md file for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program (the LICENSE.md file).  If not, see <http://www.gnu.org/licenses/>.
+#
+# To build the package
+# python setup.py build
+#
+# To install the package in the current venv
+# python setup.py install
+#
+# To build a source distribution:
+# python setup.py sdist
+#
 
 # Always prefer setuptools over distutils
-from setuptools import setup, find_packages
+from setuptools import setup
 # To use a consistent encoding
 from codecs import open
 from os import path
@@ -28,16 +54,16 @@ setup(
     long_description_content_type='text/x-rst',
 
     # The project's main homepage.
-    url='https://github.com/dhocker/Circuitpython_DotStarAPA102',
+    url='https://github.com/dhocker/CircuitPython_DotStarAPA102',
 
     # Author details
     author='Dave Hocker',
     author_email='AtHomeX10@gmail.com',
 
-    install_requires=['Adafruit-Blinka'],
+    install_requires=['Adafruit-Blinka', 'adafruit-circuitpython-busdevice'],
 
     # Choose your license
-    license='MIT',
+    license='GPLv3',
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
