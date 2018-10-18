6feaed2bfa5dc5980afc6e89257c9abd55132348
Dave Hocker <AtHomeX10@gmail.com>
2018-10-17 19:00:27 -0500

    Resolve issues with DotStar string pixels (timing). Rework example/test code.

--- examples/dotstarapa102_test.py
+++ examples/dotstarapa102_test.py
@@ -53,43 +53,81 @@ if __name__ == "__main__":
     # SCLK = #23 (clock)
     # MOSI = #19 (data out)
     # MISO = #21 (data in, not used here)
-    spi_bus = busio.SPI(board.SCLK, board.MOSI, board.MISO)
+    spi_bus = busio.SPI(board.SCLK, MOSI=board.MOSI, MISO=board.MISO)
     # Wrap the SPI bus in a context manager
     # The baudrate here is believed to be the maximum for an RPi
-    spi_dev = spi_device.SPIDevice(spi_bus, baudrate=15000000)
+    # Reference: https://www.corelis.com/education/tutorials/spi-tutorial/
+    # Reference: https://learn.adafruit.com/circuitpython-basics-i2c-and-spi/spi-devices
+    # See references for a description of polarity and phase.
+    # DotStars were found to work with either polarity=1 and phase=0 (mode 2)
+    # or polarity=0 and phase=1 (mode 1). They did not work properly with
+    # the default values polarity=0 and phase=0 (mode 0) or with 
+    # polarity=1 and phase=1 (mode 3)..
+    # Looking at the APA102C datasheet indicates that it samples the data
+    # line on the trailing edge of the clock pulse. SPI mode 1 and 2 is set up
+    # for the device to sample the data line on the falling edge of the
+    # clock which seems to be more compatible with the APA102C.
+    spi_dev = spi_device.SPIDevice(spi_bus, baudrate=15000000, polarity=1, phase=0)
     # Create DotStar driver from the context manager
     ds = DotStarAPA102(spi_dev, 30)
 
     try:
         print("Testing dotstar_apa102 package...")
+        ds.show()
+        input("Enter to start testing")
+        
         # Clear pixels
         ds.global_brightness = 4
-        print("Brightness:", ds.global_brightness)
-        ds.clear()
+        print("Brightness:", ds.global_brightness, "/ 31")
+        # ds.clear()
+        color = 0
+        for i in range(ds.num_pixels):
+            ds.clear(show=False)
+            color = color >> 8
+            if not color:
+                color = 0xFF0000
+            ds.set_pixel_color(i, color)
+            ds.show()
+            print("px", i, '%0#8x' % color)
+            # input("Enter to continue")
+            time.sleep(0.5)
         
-        # Fill all pixels
-        ds.fill_rgb(0xFF, 0, 0)
-        # Fill last 5 pixels
+        # Fill all pixels with red
+        print("All red")
+        ds.fill_rgb(255, 0, 0)
+        ds.show()
+        input("Enter to continue")
+        
+        # Fill all pixels with green
+        print("All green")
+        ds.fill_rgb(0, 0xFF, 0)
+        ds.show()
+        input("Enter to continue")
+
+        # Fill last 5 pixels with blue
+        print("Last 5 blue")
         ds.fill_rgb(0, 0, 0xFF, start=ds.num_pixels - 5, end=ds.num_pixels)
         ds.show()
-        print("Waiting 5 sec")
-        time.sleep(5.0)
+        input("Enter to continue")
         
-        # Fill first 5 pixels
-        ds.fill_rgb(0, 0xFF, 0, start=0, end=5)
+        # Fill first 5 pixels with red
+        print("5 red, 20 green, 5 blue")
+        ds.fill_rgb(255, 0, 0, start=0, end=5)
         ds.show()
-        print("Waiting 5 sec")
-        time.sleep(5.0)
+        input("Enter to continue")
         
         # Fetch some pixels
+        print("Retrieve some pixel colors")
         print(ds.get_pixel_color(4))
         print(ds.get_pixel_color(29))
         
         if color_cycler_present:
             # Try an algorithm to see what performance looks like
+            print("Running sine wave")
             sinewave(ds)
             ds.global_brightness = 8
-            print("Brightness:", ds.global_brightness)
+            print("Brightness:", ds.global_brightness, "/ 31")
+            print("Running sine wave")
             sinewave(ds)
 
     # ctrl-C exit
